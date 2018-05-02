c75659b1793fee749f000ab46ed9af20b5ecd3e9
giuseppe.donato <giuseppe.donato@mm-one.com>
2018-05-02 09:02:38 +0200

    Aggiunte funzioni per gestione delle newsletter TODO fare la versione REST seria

---
+++ app/src/Entity/Upgrade/Privacydisclaimer.php
@@ -0,0 +1,96 @@
+<?php
+namespace App\Entity\Upgrade;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Privacydisclaimer
+ *
+ * @ORM\Table(name="privacydisclaimer", indexes={@ORM\Index(name="crdate", columns={"crdate"})})
+ * @ORM\Entity
+ */
+class Privacydisclaimer
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="filename", type="string", length=255, nullable=false)
+     */
+    private $filename;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="version", type="integer", nullable=false)
+     */
+    private $version;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="crdate", type="datetime", nullable=true)
+     */
+    private $crdate;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param string $filename
+     */
+    public function setFilename(string $filename): void
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVersion(): int
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param int $version
+     */
+    public function setVersion(int $version): void
+    {
+        $this->version = $version;
+    }
+
+
+
+
+}

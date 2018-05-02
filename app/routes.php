00fbb2e3b41c9ee35464dfde215c5c7238558896
giuseppe.donato <giuseppe.donato@mm-one.com>
2018-05-02 09:01:36 +0200

    Aggiunte funzioni per gestione delle newsletter TODO fare la versione REST seria

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,16 @@ $app->get('/api/owner/term/[id]', 'App\Action\Terms:getTerm');
 $app->post('/api/auth/login', 'App\Action\Auth:login');
 $app->post('/api/auth/logout', 'App\Action\Auth:logout');
 $app->get('/api/auth/user', 'App\Action\Auth:user');
+
+
+//upgrade privacy disclaimere phase < 25th May
+
+$app->get('/upgrade/allow/{domainid}/{pathid}/{email}', 'App\Action\Subscribers:allow');
+
+$app->get('/upgrade/disallow/{domainid}/{pathid}/{email}', 'App\Action\Subscribers:disallow');
+
+$app->get('/upgrade/list/{domainid}/{pathid}/', 'App\Action\Subscribers:list');
+
+$app->get('/upgrade/domain', 'App\Action\Subscribers:domainList');
+
+$app->post('/upgrade/allow/{domainid}/{pathid}/{email}', 'App\Action\Subscribers:create');

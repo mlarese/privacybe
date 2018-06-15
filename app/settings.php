741f9557885e742b70047e715d74662e45d150b5
mauro.larese <mauro.larese@mm-one.com>
2018-06-15 15:21:14 +0200

    dataone version update

--- app/settings.php
+++ app/settings.php
@@ -90,7 +90,7 @@ return [
             'connection' => [
                 'driver'   => 'pdo_mysql',
                 'host'     => '10.0.28.1',
-                'dbname'   => 'privacy_36',
+                'dbname'   => 'privacy',
                 'user'     => 'prvcfg',
                 'password' => '7d4UXHCeRhyeWbPe',
             ]

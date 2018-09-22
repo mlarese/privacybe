f290f6b699c706c0e1c3769f3454764890e6b7f2
mlarese <mauro.larese@gmail.com>
2018-09-22 13:40:51 +0200

    courage-changes

--- app/src/Action/Modules.php
+++ app/src/Action/Modules.php
@@ -10,20 +10,28 @@ namespace App\Action;
 
 
 use App\Entity\Config\Module;
+use Slim\Http\Response;
+use Slim\Http\Request;
 
 class Modules extends AbstractAction
 {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return Response
+     */
     public function getAllModules(Request $request, Response $response, $args) {
         try {
             $em =$this->getEmConfig();
             $rep =$em->getRepository(Module::class);
-            $module = $rep->findBy();
-            return $response->withJson($this->toJson($module));
+            $module = $rep->findAll();
 
         } catch (\Exception $e) {
             echo $e->getMessage();
             return $response->withStatus(500, 'Error getting module');
         }
+        return $response->withJson($this->toJson($module));
     }
 
     public function getModule(Request $request, Response $response, $args) {
@@ -32,45 +40,78 @@ class Modules extends AbstractAction
             $id = $args['id'];
             $module = $this->getEmConfig();
             $module->getRepository(Module::class)->find($id);
-            return $response->withJson( $this->toJson($module));
 
 
         } catch (\Exception $e) {
             echo $e->getMessage();
             return $response->withStatus(500, 'Error getting module');
         }
+        return $response->withJson( $this->toJson($module));
     }
 
     public function updateModule(Request $request, Response $response, $args) {
         try {
+            $id = $args['id'];
+            /**
+             * @var Module $module
+             */
+            $module = $this->getEmConfig()->find(Module::class, $id);
+            $body = $request->getParsedBody();
+            $module->setName($body['name']);
+            $module->setOption($body['option']);
+            $module->setDescription($body['description']);
+            $module->setDeleted($body['deleted']);
+            $module->setOwnerId($body['ownerId']);
+
+            $this->getEmConfig()->persist($module);
+            $this->getEmConfig()->flush();
 
         } catch (\Exception $e) {
             echo $e->getMessage();
             return $response->withStatus(500, 'Error getting module');
         }
+        return $response->withJson($this->success());
     }
 
     public function insertModule(Request $request, Response $response, $args) {
         try {
+            /**
+             * @var Module $module
+             */
+            $module = new Module();
             $body = $request->getParsedBody();
+            $module->setName($body['name']);
+            $module->setOption($body['option']);
+            $module->setDescription($body['description']);
+            $module->setDeleted($body['deleted']);
+            $module->setOwnerId($body['ownerId']);
 
-            $this->getEmConfig()->persist();
+
+            $this->getEmConfig()->persist($module);
             $this->getEmConfig()->flush();
-
 
         } catch (\Exception $e) {
             echo $e->getMessage();
-            return $response->withStatus(500, 'Error getting module');
+            return $response->withStatus(500, 'Error inserting module');
         }
+        return $response->withJson($this->success());
     }
 
     public function moduleDelete(Request $request, Response $response, $args) {
         try {
+            $id = $args['id'];
+            /**
+             * @var Module $module
+             */
+             $module = $this->getEmConfig()->find(Module::class, $id);
+             $this->getEmConfig()->remove($module);
+             $this->getEmConfig()->flush();
 
         } catch (\Exception $e) {
             echo $e->getMessage();
-            return $response->withStatus(500, 'Error getting module');
+            return $response->withStatus(500, 'Error module');
         }
+        return $response->withJson($this->success());
     }
 
 }
